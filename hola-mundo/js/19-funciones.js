ca35e13f0c7c1e35a2886aefe7826265bacf478d
David <davidsanchezr81@gmail.com>
2018-10-06 12:19:51 +0100

    Seccion 12 - Lecture 42

---
+++ hola-mundo/js/19-funciones.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function calculadora(numero1, numero2){
+	
+	console.log("La multiplicacion de los numeros es: "+(numero1*numero2));
+}
+
+
+/* usando un for loop */
+
+for(var i=1; i<=10; i++){
+
+	console.log(i);
+	calculadora(i, 8);
+
+}
