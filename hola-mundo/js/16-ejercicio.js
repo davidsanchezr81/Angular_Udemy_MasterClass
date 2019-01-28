98ee1c5c873f0a60d07c4e285e0bded31fb4290e
David <davidsanchezr81@gmail.com>
2019-01-28 20:48:00 +0000

    Lesson 43 done

--- hola-mundo/js/16-ejercicio.js
+++ hola-mundo/js/16-ejercicio.js
@@ -1,4 +1,4 @@
-
+'use strict'
 /*
 Que nos diga que si el numero es par o impar
 1. Ventana Prompt
--- hola-mundo/js/19-funciones.js
+++ hola-mundo/js/19-funciones.js
@@ -1,16 +1,30 @@
 'use strict'
 
-function calculadora(numero1, numero2){
+function calculadora(numero1, numero2, mostrar = false){
 	
-	console.log("La multiplicacion de los numeros es: "+(numero1*numero2));
+
+	if(mostrar == false){
+		console.log("Suma " + (numero1 + numero2));
+		console.log("Resta " + (numero1 - numero2));
+		console.log("Division " + (numero1 / numero2));
+		console.log("Multiplicacion " + (numero1 * numero2));
+	}else
+	{
+	document.write("Suma " + (numero1 + numero2) + "</br>");
+	document.write("Resta " + (numero1 - numero2) + "</br>");
+	document.write("Division " + (numero1 / numero2) + "</br>");
+	document.write("Multiplicacion " + (numero1 * numero2) + "</br>");
+}
+	
 }
 
+calculadora(1,4, false);
 
 /* usando un for loop */
 
-for(var i=1; i<=10; i++){
+// for(var i=1; i<=10; i++){
 
-	console.log(i);
-	calculadora(i, 8);
+// 	console.log(i);
+// 	calculadora(i, 8);
 
-}
+// }
