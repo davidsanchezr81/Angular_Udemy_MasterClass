e1b3cebc779fdfde66a1eea2fc5fedd808a0967f
David <davidsanchezr81@gmail.com>
2019-01-30 21:09:07 +0000

    Lesson 56 done

---
+++ hola-mundo/js/26-arraymult.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var categorias = ['Terror', 'Amor', 'Accion', 'Comedia'];
+
+var peliculas = ['La vida es bella', 'La verdad duele', 'Gran Torino']
+
+var cine = [categorias, peliculas]; // este e sun array multidimensional
+
+console.log(cine[0][1]);
