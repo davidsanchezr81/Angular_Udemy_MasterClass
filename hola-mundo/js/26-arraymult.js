2ea1a33f7d52f46325c102b9bdc256f4d210e3e3
David <davidsanchezr81@gmail.com>
2019-01-31 22:08:02 +0000

    Lesson 58 done

--- hola-mundo/js/26-arraymult.js
+++ hola-mundo/js/26-arraymult.js
@@ -1,50 +1,59 @@
 'use strict'
 
-var categorias = ['Terror', 'Amor', 'Accion', 'Comedia'];
+// var categorias = ['Terror', 'Amor', 'Accion', 'Comedia'];
 
-var peliculas = ['La vida es bella', 'La verdad duele', 'Gran Torino'];
+// var peliculas = ['La vida es bella', 'La verdad duele', 'Gran Torino'];
 
-var cine = [categorias, peliculas]; // este e sun array multidimensional
+// var cine = [categorias, peliculas]; // este e sun array multidimensional
 
-console.log(cine[0][1]);
+// console.log(cine[0][1]);
 
-//--Operaciones con array
-
-
-// WHILE
-var elemento = prompt("Introduce tu pelicula");
-
-while(elemento != "Acabar"){
-    elemento = prompt("Introduce tu pelicula")
-    peliculas.push(elemento); // para meter elementos en el array
-}
-
-// DO WHILE
-do{
-    elemento = prompt("Introduce tu pelicula")
-    peliculas.push(elemento);
-} while(elemento != "Acabar");
+// //--Operaciones con array
 
 
-console.log(peliculas);
+// // WHILE
+// // var elemento = prompt("Introduce tu pelicula");
 
-peliculas.pop(); // esto elimina el ultimo elemento del array
+// // while(elemento != "Acabar"){
+// //     elemento = prompt("Introduce tu pelicula")
+// //     peliculas.push(elemento); // para meter elementos en el array
+// // }
 
-peliculas[0] = undefined; // esto es para colocarlo undefined
-
-var ubicacion = peliculas.indexOf("La vida es bella");
-
-var ubicacion1 = peliculas[2];
-
-
-/// Borrar un elemento en especifico del array
-
-var index = peliculas.indexOf("La vida es bella");
-
-if(index > -1){
-    peliculas.splice(index,2);// splice te elimina elementos de un array en referencia al de indexOf y el numero de espacios que colocas
-}
+// // // DO WHILE
+// // do{
+// //     elemento = prompt("Introduce tu pelicula")
+// //     peliculas.push(elemento);
+// // } while(elemento != "Acabar");
 
 
-var peliculas_string = peliculas.join();
-console.log(peliculas_string);
+// console.log(peliculas);
+
+// peliculas.pop(); // esto elimina el ultimo elemento del array
+
+// peliculas[0] = undefined; // esto es para colocarlo undefined
+
+// var ubicacion = peliculas.indexOf("La vida es bella");
+
+// var ubicacion1 = peliculas[2];
+
+
+// /// Borrar un elemento en especifico del array
+
+// var index = peliculas.indexOf("La vida es bella");
+
+// if(index > -1){
+//     peliculas.splice(index,2);// splice te elimina elementos de un array en referencia al de indexOf y el numero de espacios que colocas
+// }
+
+
+// var peliculas_string = peliculas.join();
+// console.log(peliculas_string);
+
+
+///--- Convertir un string a un array
+
+var cadena = "texto1, texto2, texto3";
+
+var cadena_array = cadena.split(", ");
+
+console.log(cadena_array);
